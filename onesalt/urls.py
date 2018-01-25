76b01df716ea53e86d8a819513a4e93305fb2bf6
jackzhao <zhaowangda@126.com>
2018-01-25 10:43:50 +0800

    增加URL目录

---
+++ onesalt/urls.py
@@ -0,0 +1,35 @@
+"""onesalt URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url
+from django.contrib import admin
+from saltModule import views
+
+
+
+
+urlpatterns = [
+    url(r'^admin', admin.site.urls),
+    url(r'^index', views.index),
+    url(r'^formboard', views.formboard),
+    url(r'^grouplist', views.grouplist),
+    url(r'^maintainonline', views.maintainonline),
+    url(r'^login', views.login),
+#    url(r'^', views.index),
+    url(r'datacenterlist', views.datacenterList),
+    url(r'deldatacenter', views.delDatacenter),
+    url(r'machineroomlist', views.machineRoomList),
+]
+
